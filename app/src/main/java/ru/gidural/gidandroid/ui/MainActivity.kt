501026d3f93b5d7ed15771378ccbd98755172795
dach83 <dach83@mail.ru>
2022-06-20 07:05:31 +0300

    Initial commit

---
+++ app/src/main/java/ru/gidural/gidandroid/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.gidural.gidandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gidural.gidandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
